ce96924c4b0d5ad3d8dde4b0e6ea1920edd32f49
L2az <71586138+L2az@users.noreply.github.com>
2020-10-31 16:07:48 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,16 +6,35 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
 
+import discord
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+client = discord.Client()
 
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    GLOBAL_CH_NAME = "🌸姉妹クラン募集" # グローバルチャットのチャンネル名
+    GLOBAL_WEBHOOK_NAME = "hoge-webhook" # グローバルチャットのWebhook名
 
-bot.run(token)
+    if message.channel.name == GLOBAL_CH_NAME:
+        # hoge-globalの名前をもつチャンネルに投稿されたので、メッセージを転送する
+        await message.delete()
+
+        channels = client.get_all_channels()
+        global_channels = [ch for ch in channels if ch.name == GLOBAL_CH_NAME]
+
+        for channel in global_channels:
+            ch_webhooks = await channel.webhooks()
+            webhook = discord.utils.get(ch_webhooks, name=GLOBAL_WEBHOOK_NAME)
+
+            if webhook is None:
+                # そのチャンネルに hoge-webhook というWebhookは無かったので無視
+                continue
+            await webhook.send(content=message.content,
+                username=message.author.name,
+                avatar_url=message.author.avatar_url_as(format="png"))
+
+client.run(token)
+
